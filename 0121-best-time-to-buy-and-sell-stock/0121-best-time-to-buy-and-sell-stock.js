bc188e3e6bd63c4776d124a387170a2423fb41f9
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-10-16 13:32:26 -0400

    Time: 143 ms (46.63%), Space: 51.7 MB (51.65%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+// I: array of prices of stock
+// O: maximum profit
+// C: cannot sell before buy
+// E: no profit -> 0
+
+// local min, local max, global max
+// loop through prices
+    // if current is lower than local min, local min and max are current
+    // if current is greater than local max, update local max
+    // if profit (max - min) is greater than global max, update
+var maxProfit = function(prices) {
+    var min = prices[0]
+    var max = prices[0]
+    var globalMax = 0;
+    for (var i = 0; i < prices.length; i++) {
+        if (prices[i] < min) {
+            min = prices[i];
+            max = prices[i];
+        }
+        if (prices[i] > max) {
+            max = prices[i];
+        }
+        var profit = max - min;
+        globalMax = Math.max(profit, globalMax);
+    }
+    return globalMax;
+};
