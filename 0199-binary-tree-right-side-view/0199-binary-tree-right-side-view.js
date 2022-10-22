5541362617b182cb029969799cc8411b7eb9748d
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-10-22 01:18:22 -0400

    Time: 97 ms (64.31%), Space: 43.6 MB (71.01%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -12,21 +12,36 @@
  */
 
 // BFS and result.push the last node of each level
+// var rightSideView = function(root) {
+//     if (!root) return [];
+//     var result = [];
+//     var queue = [root];
+//     while (queue.length) {
+//         let length = queue.length;
+//         let counter = 0;
+//         var curr;
+//         while (counter < length) {
+//             curr = queue.shift();
+//             if (curr.left) queue.push(curr.left);
+//             if (curr.right) queue.push(curr.right);
+//             counter++;
+//         }
+//         result.push(curr.val)
+//     }
+//     return result;
+// };
+
+// DFS (preorder with right first), keep track of levels
 var rightSideView = function(root) {
     if (!root) return [];
     var result = [];
-    var queue = [root];
-    while (queue.length) {
-        let length = queue.length;
-        let counter = 0;
-        var curr;
-        while (counter < length) {
-            curr = queue.shift();
-            if (curr.left) queue.push(curr.left);
-            if (curr.right) queue.push(curr.right);
-            counter++;
+    function traverse(node, level) {
+        if (level > result.length) {
+            result.push(node.val);
         }
-        result.push(curr.val)
+        if (node.right) traverse(node.right, level + 1);
+        if (node.left) traverse(node.left, level + 1)
     }
+    traverse(root, 1);
     return result;
-};
+}
