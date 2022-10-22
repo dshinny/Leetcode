21e281c6752c456e28ae76cc4fc0dfdbbbfae249
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-10-22 01:03:38 -0400

    Time: 83 ms (92.18%), Space: 53.4 MB (16.82%) - LeetHub

--- 0134-gas-station/0134-gas-station.js
+++ 0134-gas-station/0134-gas-station.js
@@ -4,13 +4,16 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
+    var diffArr = [];
+    for (var i = 0; i < gas.length; i++) {
+        diffArr[i] = gas[i] - cost[i];
+    }
     var tank = 0;
     var start = 0;
     var curr = 0;
-    for (var i = 0; i < gas.length; i++) {
-        var diff = gas[i] - cost[i];
-        tank += diff;
-        curr += diff;
+    for (var i = 0; i < diffArr.length; i++) {
+        tank += diffArr[i];
+        curr += diffArr[i];
         if (curr < 0) {
             start = i + 1;
             curr = 0
