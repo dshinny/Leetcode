449fb6aa0589f0e9c32b746facb83de79234729d
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-10-25 16:43:24 -0400

    Time: 154 ms (35.91%), Space: 50 MB (35.89%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -0,0 +1,41 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+// initialize counter
+// sequential iteration through every element
+    // if 1, increment counter and use bfs to turn every adjacent 1's to 0's
+var numIslands = function(grid) {
+    var counter = 0;
+    for (var i = 0; i < grid.length; i++) {
+        for (var j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === '1') {
+                counter++;
+                turnIslandIntoWater(grid, i, j);
+            }
+        }
+    }
+    return counter;
+};
+
+var turnIslandIntoWater = function(grid, startRow, startCol) {
+    const directions = [
+        [-1, 0],
+        [1, 0],
+        [0, 1],
+        [0, -1]
+    ]
+    var queue = [[startRow, startCol]];
+    while (queue.length) {
+        var curr = queue.shift();
+        var row = curr[0];
+        var col = curr[1];
+        if (row < '0' || col < '0' || row >= grid.length || col >= grid[0].length || grid[row][col] === '0') continue;
+        grid[row][col] = '0';
+        for (var i = 0; i < directions.length; i++) {
+            var currDir = directions[i];
+            queue.push([row + currDir[0], col + currDir[1]])
+        }
+    }
+}
