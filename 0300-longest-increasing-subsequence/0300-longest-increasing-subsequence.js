4774ef202975a4e4bbcc8d985bf16d0f597d5977
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-11-03 10:52:41 -0400

    Time: 211 ms (64.42%), Space: 44 MB (40.52%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -3,19 +3,13 @@
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
-    var result = 1;
     var dp = new Array(nums.length).fill(1);
     for (var i = nums.length - 1; i >= 0; i--) {
-        var start = nums[i]
         for (var j = i; j < nums.length; j++) {
-            var curr = nums[j];
-            if (curr > start) {
+            if (nums[j] > nums[i]) {
                 dp[i] = Math.max(dp[i], 1 + dp[j]);
-                result = Math.max(dp[i], result);
             }
         }
-        dp[i] = Math.max(dp[i], 1)
-        result = Math.max(result, dp[i])
     }
-    return result;
+    return Math.max(...dp)
 };
