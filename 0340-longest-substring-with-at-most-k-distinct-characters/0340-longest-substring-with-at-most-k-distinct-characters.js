74a2b00a3f467713646405db5f9ab3e872c93b9e
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-11-04 23:29:51 -0400

    Time: 1237 ms (5.32%), Space: 48.9 MB (11.41%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.js
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.js
@@ -6,26 +6,23 @@
 var lengthOfLongestSubstringKDistinct = function(s, k) {
     if (k === 0) return 0;
     var key = {};
-    var left = 0;
-    var right = 0;
-    key[s[left]] = 1;
+    var start = 0;
     var max = 1;
-    while (right < s.length) {
-        if (Object.values(key).length > k) {
-            key[s[left]]--;
-            if (key[s[left]] === 0) {
-                delete key[s[left]];
-            }
-            left++;
-        } else {
-            max = Math.max(max, right - left + 1)
-            right++;
-            if (key[s[right]]) {
-                key[s[right]]++;
-            } else {
-                key[s[right]] = 1;
-            }
+    for (var end = 0; end < s.length; end++) {
+        const rightChar = s[end];
+        if (!(rightChar in key)) {
+            key[rightChar] = 0;
         }
+        key[rightChar]++;
+        while (Object.values(key).length > k) {
+            const leftChar = s[start];
+            key[leftChar]--;
+            if (key[leftChar] === 0) {
+                delete key[leftChar];
+            }
+            start++;
+        }
+        max = Math.max(max, end - start + 1)
     }
     return max;
 };
