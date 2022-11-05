ffbe6ab67947152cfcc02f7cef91ec211d9bddcf
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-11-04 20:59:16 -0400

    Time: 63 ms (95.07%), Space: 41.9 MB (83.17%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -6,7 +6,6 @@
 var uniquePaths = function(m, n) {
     var row = 0;
     var col = 0;
-    var counter = 0;
     var dp = new Array(m).fill(0).map(() => new Array(n).fill(1));
     for (var i = m - 2; i >= 0; i--) {
         for (var j = n - 2; j >= 0; j--) {
