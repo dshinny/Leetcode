6b03539fed0af8d04dadc9eb1e0565b7c42fa4fb
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-11-08 17:10:27 -0500

    Time: 80 ms (77.99%), Space: 42 MB (70.53%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
@@ -12,49 +12,32 @@
  * @return {ListNode}
  */
 
-// I: head of SLL
-// O: head of reversed list
-// C: none
-// E: reversing the entire list
-
-// intiailize prev, curr, next, position = 1
-// initialize before, after, subHead, last
-// loop through until right
-    // if position is left
-        // before = prev
-        // subHead = curr
-    // if position is greater than left
-        // reverse
-    // update variables
-    // increment position
-// connect sublist with list
-// edge case for left === 1
-
-
 var reverseBetween = function(head, left, right) {
+    if (!head) return null;
     var prev = null;
     var curr = head;
     var next;
-    var position = 1;
-    var head = head;
-    var before, after, subHead, last
-    while (position <= right) {
-        var next = curr.next;
-        if (position === left) {
+    var index = 1;
+    var subTail;
+    var before;
+    while (index <= right) {
+        next = curr.next;
+        if (index === left) {
+            subTail = curr;
             before = prev;
-            subHead = curr;
         }
-        if (position > left) {
-            curr.next = prev;
+        if (index > left) {
+            curr.next = prev;  
         }
         prev = curr;
         curr = next;
-        position++;
+        index++;
     }
-    if (before) before.next = prev;
-    subHead.next = curr;
-    if (left === 1) {
-        head = prev;
+    subTail.next = curr;
+    if (before) {
+        before.next = prev;
+    } else {
+        return prev;
     }
     return head;
 };
