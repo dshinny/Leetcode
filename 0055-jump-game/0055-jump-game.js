0493a550538e364cb82cb15e9e809437d1be5413
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-11-23 20:41:15 -0500

    Time: 78 ms (94.76%), Space: 45.9 MB (91.68%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -3,28 +3,11 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    if (nums.length <= 1) return true;
-    var dp = new Array(nums.length).fill(undefined);
-    dp[dp.length - 1] = true;
-    var traverse = function(index) {
-        if (dp[index] !== undefined) {
-            return dp[index];
-        }
-        if (nums[index] === 0) {
-            dp[index] = false;
-            return false;
-        }
-        for (var n = 1; n <= nums[index]; n++) {
-            if(traverse(index + n)) {
-                dp[index] = true;
-                return true;
-            }
-        }
-        if (dp[index] === undefined) {
-            dp[index] = false;
-            return false;
+    var goalIndex = nums.length - 1;
+    for (var i = nums.length - 2; i >= 0; i--) {
+        if (i + nums[i] >= goalIndex) {
+            goalIndex = i;
         }
     }
-    traverse(0);
-    return dp[0];
+    return goalIndex === 0;
 };
