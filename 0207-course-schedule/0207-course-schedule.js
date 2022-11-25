c599f9865787cf4750b93b5401098d263b83f762
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-11-25 17:16:09 -0500

    Time: 127 ms (61.06%), Space: 45.4 MB (92.55%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -4,27 +4,27 @@
  * @return {boolean}
  */
 var canFinish = function(numCourses, prerequisites) {
-    var inDegree = new Array(numCourses).fill(0);
-    var list = inDegree.map(() => []);
+    var indegree = new Array(numCourses).fill(0);
+    var list = indegree.map(() => [])
     for (var i = 0; i < prerequisites.length; i++) {
         var pair = prerequisites[i];
-        inDegree[pair[0]]++;
         list[pair[1]].push(pair[0]);
+        indegree[pair[0]]++;
     }
     var stack = [];
     var count = 0;
-    for (var i = 0; i < inDegree.length; i++) {
-        if (inDegree[i] === 0) {
+    for (var i = 0; i < indegree.length; i++) {
+        if (indegree[i] === 0) {
             stack.push(i);
         }
     }
     while (stack.length) {
         var curr = stack.pop();
         count++;
-        const adjacentArr = list[curr];
-        adjacentArr.forEach(neighbor => {
-            inDegree[neighbor]--;
-            if (inDegree[neighbor] === 0) {
+        const adj = list[curr]
+        adj.forEach(neighbor => {
+            indegree[neighbor]--;
+            if (indegree[neighbor] === 0) {
                 stack.push(neighbor);
             }
         })
