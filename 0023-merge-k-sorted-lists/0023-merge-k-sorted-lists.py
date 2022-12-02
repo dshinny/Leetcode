5ab96c9df5d9ed411bda66c5d8a239742655b78a
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-12-02 09:37:56 -0500

    Time: 228 ms (54.43%), Space: 18.4 MB (27.76%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -6,22 +6,16 @@
 import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
-            return None
+        prehead = ListNode(0)
         minHeap = []
         for head in lists:
-            if not head:
-                continue
             while head:
                 heapq.heappush(minHeap, head.val)
                 head = head.next
-        if not minHeap:
-            return None
-        newHead = ListNode(heapq.heappop(minHeap))
-        curr = newHead
+        curr = prehead
         while minHeap:
             removed = heapq.heappop(minHeap)
             newNode = ListNode(removed)
             curr.next = newNode
             curr = newNode
-        return newHead
+        return prehead.next
