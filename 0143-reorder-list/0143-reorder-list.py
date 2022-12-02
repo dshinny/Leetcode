f62ef9dafdd32f69686708f9284fa6addda2e058
Daniel Shin <104472508+dshinny@users.noreply.github.com>
2022-12-02 09:09:08 -0500

    Time: 184 ms (58.29%), Space: 23.9 MB (64.98%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,24 +8,35 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        # find middle node
         if head is None:
             return None
-        arr = collections.deque()
-        curr = head.next
-        prev = head
+        fast = head
+        middle = head
+        midPrev = ListNode(0)
+        while fast and fast.next:
+            fast = fast.next.next
+            midPrev = middle
+            middle = middle.next
+        midPrev.next = None
+        # reverse from middle node
+        curr = middle
+        prev = None
         while curr:
-            arr.append(curr)
-            prev.next = None
+            next = curr.next
+            curr.next = prev
             prev = curr
-            curr = curr.next
-        curr = head
-        counter = 1
-        while arr:
-            if counter % 2 == 0:
-                removed = arr.popleft()
-            else:
-                removed = arr.pop()
-            counter +=1
-            curr.next = removed
-            curr = removed
+            curr = next
+        
+        # merge two lists
+        l1 = head
+        l2 = prev
+        while l1 and l2:
+            n1, n2 = l1.next, l2.next
+            l1.next = l2
+            last = l2
+            l2.next = n1
+            l1, l2 = n1, n2
+        if l2:
+            last.next = l2
         return head
